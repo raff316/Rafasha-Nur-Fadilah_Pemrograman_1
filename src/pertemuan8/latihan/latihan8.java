537908e3acf2d00dc1f4b278edf5fd851b3dc020
raff316 <raff030106@gmail.com>
2026-05-04 23:32:16 +0700

    update

--- src/pertemuan8/latihan/latihan8.java
+++ src/pertemuan8/latihan/latihan8.java
@@ -33,20 +33,16 @@ public class latihan8 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
     
-        // 1. For Loop: Perfect for when you know the exact range.
         for(int a = 2; a <= 4; a++){
             System.out.println("pengulangan for" + a);
         }
         
-        // 2. While Loop: Fixed. 'b' must be declared and initialized outside.
         int b = 1; 
         while(b <= 5){
             System.out.println("pengulangan while " + b);
             b++;
         }
 
-        // 3. Do-While Loop: This is correct. 
-        // It will always run at least once because the condition is checked at the end.
         int c = 1;
         do { 
             System.out.println("Pengulangan do " + c);
